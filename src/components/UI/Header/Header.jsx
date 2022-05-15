6648030434b64b565cb1d675b52ef0f4c3a75b47
PuneetP16 <patelpunit95@gmail.com>
2022-05-16 03:11:25 +0530

    update: logo & favicon

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
 import { CTA, SearchBox } from "../../../components";
+import { icon, magnificent } from "../../../data/Logo/logo";
 import "./Header.css";
 
 export const Header = () => {
 	return (
 		<header className="header">
 			<Link className="brand__text grid-center" to="/">
-				Magnificent Tube
+				<img className="logo__img" src={icon} alt="logo" />
+				Play
 			</Link>
 			<SearchBox />
 			<CTA />
